7cbc472a657f979127ea3b458e680999ab4dc3b5
EmmaNuelsync <emmysquad11@gmail.com>
2025-10-29 13:08:27 +0100

    Project Kick-off

--- sms/routes/web.php
+++ sms/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Or if using custom routes
+Route::get('register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
